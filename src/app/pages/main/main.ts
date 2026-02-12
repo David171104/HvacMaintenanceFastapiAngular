cd796ff8ab38075c2be70361da95bc71015e7c37
David171104 <altamardavid8@qgmail.com>
2026-02-11 22:14:40 -0500

    Add principal styles of main page

--- src/app/pages/main/main.ts
+++ src/app/pages/main/main.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { Navbar } from '../../components/navbar/navbar';
+import { Footer } from '../../components/footer/footer';
+
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  standalone: true,
+  imports: [Navbar, Footer],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
-export class Main {
-
-}
+export class Main {}
