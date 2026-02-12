4f2aa950f7d98c352f8fd5415b12b9273362cb86
David171104 <altamardavid8@qgmail.com>
2026-02-11 22:00:00 -0500

    first commit- (Add a principal components and pages)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Main } from './pages/main/main';
+import { Contact } from './pages/contact/contact';
+import { Notfound } from './pages/notfound/notfound';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:Main},
+    {path:'contact', component:Contact},
+    {path:'**', component:Notfound}
+
+
+];
