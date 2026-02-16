8bdd23c590be6afc47d7f2bda8f668797664d1c0
David171104 <altamardavid8@qgmail.com>
2026-02-16 17:01:20 -0500

    Feat a services component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,17 @@ import { Contact } from './pages/contact/contact';
 import { Notfound } from './pages/notfound/notfound';
 import { RegisterComponent } from './pages/register/register';
 import { Users } from './pages/users/users';
+import { Services } from './pages/services/services';
 import { LoginComponent } from './pages/login/login';
 
 export const routes: Routes = [
-    {path:'', component:Main},
-    {path:'contact', component:Contact},
+    { path: '', component:Main},
+    { path: 'contact', component:Contact},
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
     { path: 'users', component: Users },
-    {path:'**', component:Notfound}
+    { path: 'services', component: Services },
+    { path: '**', component:Notfound}
 
 
 ];
